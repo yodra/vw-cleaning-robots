d96f44cef851827e7296ca647f417996f9756684
yodralopez <yodralopez@gmail.com>
2025-02-02 10:46:58 +0000

    initial commit

---
+++ src/main/java/com/yodralopez/vwcleaningrobots/VwCleaningRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.yodralopez.vwcleaningrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwCleaningRobotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VwCleaningRobotsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yodralopez/vwcleaningrobots/VwCleaningRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yodralopez.vwcleaningrobots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VwCleaningRobotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
