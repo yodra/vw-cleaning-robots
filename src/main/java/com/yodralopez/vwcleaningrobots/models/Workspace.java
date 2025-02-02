d4cd5ead7fa96d0c9d71b2368cfa34c80eef99e6
yodralopez <yodralopez@gmail.com>
2025-02-02 12:51:51 +0000

    add methods to check the workspace bounds

--- src/main/java/com/yodralopez/vwcleaningrobots/models/Workspace.java
+++ src/main/java/com/yodralopez/vwcleaningrobots/models/Workspace.java
@@ -1,6 +1,9 @@
 package com.yodralopez.vwcleaningrobots.models;
 
 public class Workspace {
+    private static final int BOTTOM = 0;
+    private static final int LEFT = 0;
+
     private final int width;
     private final int height;
 
@@ -11,5 +14,17 @@ public class Workspace {
 
     public static Workspace create(int width, int height) {
         return new Workspace(width, height);
+    }
+
+    public boolean isWithinBounds(int x, int y) {
+        return isWithinWidth(x) && isWithinHeight(y);
+    }
+
+    private boolean isWithinWidth(int x) {
+        return x >= BOTTOM && x <= width;
+    }
+
+    private boolean isWithinHeight(int y) {
+        return y >= LEFT && y <= height;
     }
 }
