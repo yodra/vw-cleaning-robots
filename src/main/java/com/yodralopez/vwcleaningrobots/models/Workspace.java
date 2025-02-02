bfc02757dfe45b731271ce5d05cdc7a5a4ff38b1
yodralopez <yodralopez@gmail.com>
2025-02-02 12:40:07 +0000

    refactor rename Workspace attributes

--- src/main/java/com/yodralopez/vwcleaningrobots/models/Workspace.java
+++ src/main/java/com/yodralopez/vwcleaningrobots/models/Workspace.java
@@ -1,15 +1,15 @@
 package com.yodralopez.vwcleaningrobots.models;
 
 public class Workspace {
-    private final int maxX;
-    private final int maxY;
+    private final int width;
+    private final int height;
 
-    private Workspace(int maxX, int maxY) {
-        this.maxX = maxX;
-        this.maxY = maxY;
+    private Workspace(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 
-    public static Workspace create(int maxX, int maxY) {
-        return new Workspace(maxX, maxY);
+    public static Workspace create(int width, int height) {
+        return new Workspace(width, height);
     }
 }
